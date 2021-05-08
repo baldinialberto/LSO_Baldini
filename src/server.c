58a029b1473f091e17774fe9995b1892a9e13bad
Alberto Baldini <albertobaldini@Albertos-MacBook-Pro.local>
2021-05-08 20:33:56 +0200

    TigreProva

--- src/server.c
+++ src/server.c
@@ -4,6 +4,6 @@ int main(int argc, char** argv)
 {
 	server_settings settings = parse_settings();
 	print_server_settings(&settings);
-	write_log("Ciao Mamma", &settings);
+	write_log("Tigre Sul Puff", &settings);
 	return EXIT_SUCCESS;
 }
