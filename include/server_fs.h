e4a6f91901a71ed4d7de42546124e6a2cf1f8e2f
baldinialberto <baldini.alberto.95@gmail.com>
2021-05-17 07:44:11 +0000

    wip

--- include/server_fs.h
+++ include/server_fs.h
@@ -12,26 +12,22 @@
 #include "common.h"
 #include "error_utils.h"
 
-#define SFS_INODE_FC        0xFF000000
-// inode mask defines
-#define SFS_INODE_F1        0x00FF0000
-#define SFS_INODE_F2        0x0000FF00
-#define SFS_INODE_F3        0x000000FF
-#define SFS_INODE_F1RS      0x00000008
-#define SFS_INODE_F2RS      0x00000010
-#define SFS_INODE_F3RS      0x00000018
+#define SFS_PNODE_PRES_BIT      0b10000000
+#define SFS_PNODE_USE_BIT       0b01000000
+#define SFS_PNODE_INODE_BIT     0b00100000
+#define SFS_PNODE_LINK_BIT      0b00010000
+#define SFS_PNODE_COUNT_BITS    0b00001111
 
-#define SFS_INODE_FLOCK     0b10000000
-#define SFS_INODE_FOPEN     0b01000000
-#define SFS_INODE_FWRITE    0b00100000
-#define SFS_INODE_FDIR      0b00010000
-#define SFS_INODE_ACCESS    0b00001111
+#define SFS_FNODE_USE_BIT       0b10000000
+#define SFS_FNODE_LOCK_BIT      0b01000000
+#define SFS_FNODE_OPEN_BIT      0b00100000
+#define SFS_FNODE_WRITE_BIT     0b00010000
+#define SFS_FNODE_USR1_BIT      0b00001000
+#define SFS_FNODE_USR2_BIT      0b00000100
+#define SFS_FNODE_USR3_BIT      0b00000010
+#define SFS_FNODE_USR4_BIT      0b00000001
 
-#define SFS_FNODE_SIZE      0x0154
-#define SFS_FNODE_CHARS     0x0148
-
-#define SFS_PAGE_SIZE       0x0400
-#define SFS_PAGED_SIZE      0x03F8
+#define SFS_FNODE_
 
 #define SFS_PAGE    struct _page   
 #define SFS_FILE    struct _file
@@ -49,19 +45,20 @@
 
 struct _file
 {
-    char filename[SFS_FNODE_CHARS];
-    int page;
+    int namelen;
     int size;
+    char *filename;
+    char fnode;
 };
 struct _page
 {
-    int inode;
+    char pnode;
     char *data;
-    int next;
 };
 struct _fs
 {
     struct _page *memory;
+    int freepages;
     int pagecount;
     int max_filecount;
 };
--- src/server_fs.c
+++ src/server_fs.c
@@ -2,13 +2,11 @@
 
 int init_server_fs(SFS_FS *fs, int avaiableMemory, int maxFileCount)
 {
-    // each file needs 1/3 page for name, pos and size
-    // and at least 1 page for storage 
-    int minKB = (maxFileCount / 3) + 1 + maxFileCount;
-    if (minKB > avaiableMemory * 1024) return 1;
+    // each file needs at least 1 page for storage 
+    if (maxFileCount > (avaiableMemory * 1024)) return 1;
 
     fs->max_filecount = maxFileCount;
-    fs->pagecount = avaiableMemory * 1024;
+    fs->pagecount = (fs->freepages = avaiableMemory * 1024);
     CHECK_BADVAL_PERROR_EXIT(
         fs->memory = calloc(fs->pagecount, sizeof(SFS_PAGE)), 
         NULL, "init_server_fs : calloc"
