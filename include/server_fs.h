129b7bc4d0fd3a581541b8bc95b1f1e242d093b1
baldinialberto <baldini.alberto.95@gmail.com>
2021-05-20 17:25:54 +0200

    wip

--- include/server_fs.h
+++ include/server_fs.h
@@ -1,97 +1,36 @@
-#ifndef _SERVER_FS_H
-#define _SERVER_FS_H
+#ifndef SERVER_FS
+#define SERVER_FS
 
 #pragma once
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-#include "lock_utils.h"
-#include "cond_utils.h"
-#include "common.h"
-#include "error_utils.h"
+#include <error_utils.h>
+#include <common.h>
+#include <commonio.h>
+#include <lock_utils.h>
+#include <cond_utils.h>
 
-#define SFS_PNODE_PRES_BIT      0b10000000
-#define SFS_PNODE_USE_BIT       0b01000000
-#define SFS_PNODE_INODE_BIT     0b00100000
-#define SFS_PNODE_LINK_BIT      0b00010000
-#define SFS_PNODE_COUNT_BITS    0b00001111
-
-#define SFS_FNODE_USE_BIT       0b10000000
-#define SFS_FNODE_LOCK_BIT      0b01000000
-#define SFS_FNODE_OPEN_BIT      0b00100000
-#define SFS_FNODE_WRITE_BIT     0b00010000
-#define SFS_FNODE_USR1_BIT      0b00001000
-#define SFS_FNODE_USR2_BIT      0b00000100
-#define SFS_FNODE_USR3_BIT      0b00000010
-#define SFS_FNODE_USR4_BIT      0b00000001
-
-#define SFS_PAGESPACE_CONT      0x03FB
-#define SFS_PAGESPACE           0x03FF
-
-#define SFS_PAGE    struct _page   
-//#define SFS_FILE    struct _file
-#define SFS_FS      struct _fs
-
-#define SFS_WRITE           0x01
-#define SFS_APPEND          0x02
-#define SFS_START           0x00
-#define SFS_CURR            0x01
-#define SFS_EOF             0x02
-
-#define SFS_SUCESS          0x00
-#define SFS_FNF             0x01
-#define SFS_MEM_FULL        0x02
-
-/*
-struct _file
+typedef struct _sfs_folder
 {
-    int namelen;
-    int size;
-    char *filename;
-    char fnode;
-};
-*/
-struct _page
+
+} sfs_folder;
+typedef struct _sfs_file
 {
-    unsigned char pnode;
-    char *data;
-};
-struct _fs
-{
-    struct _page *memory;
-    int freepages;
-    int pagecount;
-    int max_filecount;
-};
+
+} sfs_file;
 typedef struct _sfs_fd
 {
-    int file;
-    int fptr;
-    int size;
+
 } sfs_fd;
+typedef struct _sfs_fs
+{
+    int currentMB;
+    int maxMB;
+    int currentFiles;
+    int maxFiles;
+    mutex_t lock;
 
-/**
- * @brief initiate the server filesystem according to thespecifications
- * @param avaiableMemory    server's memory capacity in MB
- * @param maxFileCount      server's file capacity
- * @return 0 on success, > 0 otherwise
- */
-int init_server_fs(SFS_FS *fs, int avaiableMemory, int maxFileCount);
-
-sfs_fd* sfs_create(SFS_FS *fs, char *path, void *buff, size_t size);
-
-sfs_fd* sfs_open(SFS_FS *fs, char *path);
-
-int sfs_close(SFS_FS *fs, sfs_fd *file);
-
-int sfs_read(SFS_FS *fs, sfs_fd *file, void *buff, size_t size);
-
-int sfs_write(SFS_FS *fs, sfs_fd *file, void *buff, size_t size);
-
-int sfs_evictpages(SFS_FS *fs, int npages);
-
-int sfs_pages_required_byfile(char *path, size_t size);
+} sfs_fs;
 
 #endif
--- src/server_paged_fs.c
+++ src/server_paged_fs.c
@@ -1,29 +1,29 @@
-#include <server_fs.h>
+#include <server_paged_fs.h>
 
 int init_server_fs(SFS_FS *fs, int avaiableMemory, int maxFileCount)
 {
-    // each file needs at least 1 page for storage 
-    if (maxFileCount > (avaiableMemory * 1024)) return 1;
+    // each file needs at least 1 page for storage
+    if (maxFileCount > (avaiableMemory * 1024))
+        return 1;
 
     fs->max_filecount = maxFileCount;
     fs->pagecount = (fs->freepages = avaiableMemory * 1024);
     CHECK_BADVAL_PERROR_EXIT(
-        fs->memory = calloc(fs->pagecount, sizeof(SFS_PAGE)), 
-        NULL, "init_server_fs : calloc"
-    );
+        fs->memory = calloc(fs->pagecount, sizeof(SFS_PAGE)),
+        NULL, "init_server_fs : calloc");
 
     return 0;
 }
 
-sfs_fd* sfs_create(SFS_FS *fs, char *path, void *buff, size_t size)
+sfs_fd *sfs_create(SFS_FS *fs, char *path, void *buff, size_t size)
 {
     if (fs == NULL || path == NULL || buff == NULL)
-        return (sfs_fd*)-1;
-    
+        return (sfs_fd *)-1;
+
     int requiredpages = sfs_pages_required_byfile(path, size);
     sfs_fd *newFile = NULL;
 
-    if (requiredpages > fs->freepages) 
+    if (requiredpages > fs->freepages)
         sfs_evictpages(fs, requiredpages - fs->freepages);
 
     for (int i = 0; i < fs->pagecount; i++)
@@ -34,11 +34,8 @@ sfs_fd* sfs_create(SFS_FS *fs, char *path, void *buff, size_t size)
             (fs->memory)[i].pnode |= SFS_PNODE_INODE_BIT;
             if (requiredpages > 1)
                 (fs->memory)[i].pnode |= SFS_PNODE_LINK_BIT;
-            
         }
     }
-
-
 }
 
 int sfs_pages_required_byfile(char *path, size_t size)
@@ -52,8 +49,8 @@ int sfs_pages_required_byfile(char *path, size_t size)
         size -= (SFS_PAGESPACE_CONT - filedeflen - 1);
         requiredpages++;
         size -= SFS_PAGESPACE;
-        requiredpages += size / SFS_PAGESPACE_CONT + 
-            (size % SFS_PAGESPACE_CONT > 0);
+        requiredpages += size / SFS_PAGESPACE_CONT +
+                         (size % SFS_PAGESPACE_CONT > 0);
     }
 
     return requiredpages;
