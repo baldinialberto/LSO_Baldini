f82cad7c96e265b84745e0bbb248ca645eaf3f88
baldinialberto <baldini.alberto.95@gmail.com>
2021-05-24 13:21:02 +0000

    wip

--- clienttest.sh
+++
@@ -1,4 +0,0 @@
-for i in {1..600}
-do
-    ./client.exe -f 'MyServerSocket.socket' -w '.',10 -W 'pippo.txt' -D 'evicted' -r 'pluto.txt' -R 10 -d 'readen' -t 10 -l 'pippo.txt','pluto.txt' -u 'pippo.txt','pluto.txt' -c 'pippo.txt' -p &
-done
