db35d3d276b2b6822d97bbf85a9737fc441da5dd
baldinialberto <baldini.alberto.95@gmail.com>
2021-05-25 10:22:39 +0000

    wip

--- BETA/include/hash_utils.h
+++ BETA/include/hash_utils.h
@@ -1,6 +1,12 @@
 #ifndef _HASH_UTILS_H
 #define _HASH_UTILS_H
 
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+#include "mem_utils.h"
+#include "list_utils.h"
+
 #define hu_foreach(table, do)
 
 typedef struct _utils_hash_table
@@ -10,19 +16,20 @@ typedef struct _utils_hash_table
     size_t datasize;
     size_t (*hashfunc)(void *key);
     int (*datacompare)(void *a, void *b);
+    void *(*extractkey)(void *data);
     void (*dataprint)(void *data);
     void (*datafree)(void *data);
 } u_hash_table;
 
 u_hash_table hu_init(size_t nentries, size_t datasize, 
     size_t (*hashfunc)(void *key), int (*datacompare)(void *a, void *b), 
+    void *(*extractkey)(void *data), 
     void (*dataprint)(void *data), void (*datafree)(void *data));
-
-int hu_ishere(u_hash_table *table, void *data);
-void *hu_search(u_hash_table *table, void *data);
+int hu_ishere(u_hash_table *table, void *key);
 int hu_insert(u_hash_table *table, void *data);
 int hu_insert_oncopy(u_hash_table *table, void *data);
-int hu_remove(u_hash_table *table, void *data);
+int hu_remove(u_hash_table *table, void *key);
+void *hu_get(u_hash_table *table, void *key);
 void hu_free(u_hash_table *table);
 void hu_print(u_hash_table *table);
 
--- BETA/src/hash_utils.c
+++ BETA/src/hash_utils.c
@@ -1,3 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "../include/hash_utils.h"
+
+u_hash_table hu_init(size_t nentries, size_t datasize, 
+    size_t (*hashfunc)(void *key), int (*datacompare)(void *a, void *b), 
+    void *(*extractkey)(void *data), 
+    void (*dataprint)(void *data), void (*datafree)(void *data))
+{
+    u_hash_table table;
+    table.nentries = nentries;
+    table.datasize = datasize;
+    table.hashfunc = hashfunc;
+    table.datacompare = datacompare;
+    table.extractkey = extractkey;
+    table.dataprint = dataprint;
+    table.datafree = datafree;
+    table.table = mu_calloc(sizeof(u_list) * nentries);
+
+    return table;
+}
+int hu_ishere(u_hash_table *table, void *key)
+{
+    return 0;
+}
+int hu_insert(u_hash_table *table, void *data)
+{
+    return 0;
+}
+int hu_insert_oncopy(u_hash_table *table, void *data)
+{
+    return 0;
+}
+int hu_remove(u_hash_table *table, void *key)
+{
+    return 0;
+}
+void *hu_get(u_hash_table *table, void *key)
+{
+    return NULL;
+}
+void hu_free(u_hash_table *table)
+{
+
+}
+void hu_print(u_hash_table *table)
+{
+
+}
