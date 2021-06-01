1aefd9a54ee8df67a2b29ff328707d12cafded09
baldinialberto <baldini.alberto.95@gmail.com>
2021-06-01 10:03:03 +0200

    wip

--- libs/LSO_LIB/array_utils.h
+++ libs/LSO_LIB/array_utils.h
@@ -12,14 +12,15 @@ typedef struct u_arr
     void *data;
     size_t len;
     size_t elementSize;
-    int (*compare)(void *, void *)
+    int (*compare)(void *, void *);
+    void (*print)(void *);
 } u_arr;
 
 u_arr au_init_arr(size_t len, size_t elementSize, int(*compare)(void *, void *));
 void au_free(u_arr *arr);
 int au_insert(u_arr *arr, void *newElement);
 int au_remove(u_arr *arr, void *oldElement);
-
-
+int au_print(u_arr *arr);
+void *au_get(u_arr *arr, size_t i);
 
 #endif //LSO_LIB_ARRAY_UTILS_H
