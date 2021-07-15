d6ca0beda88776f74e8f10ebef03a6feb252732a
baldinialberto <baldini.alberto.95@gmail.com>
2021-07-15 21:15:23 +0200

    wip

--- TEST/include/sys_utils.h
+++ TEST/include/sys_utils.h
@@ -4,6 +4,33 @@
 #include <sys/types.h>
 
 /**
+ * Macro to handle read errors
+ */
+#define su_read_error_handler(su_read, conn_reset, not_conn, e_fault, bad_fd) \
+if ((su_read)==-1){                                                           \
+    switch (errno)                                                            \
+    {                                                                         \
+        case ECONNRESET:                                                      \
+            conn_reset;                                                       \
+            break;                                                            \
+        case ENOTCONN:                                                        \
+            not_conn;                                                         \
+            break;                                                            \
+        case EFAULT:                                                          \
+            e_fault;                                                          \
+            break;                                                            \
+        case BADFD:                                                           \
+            badfd;                                                            \
+            break;                                                            \
+    }                                                                         \
+}
+
+/**
+ * Macro to handle write errors
+ */
+#define su_write_error_handler(su_write, )
+
+/**
  * Wrapper for function read
  * @param fd file descriptor to read from
  * @param buff buffer to read data to
